3744420b60c5f13358fb54df2bbc8e51b14c533c
nazmul_avanto <nazmul.avanto@gmail.com>
2019-05-28 17:13:34 +0200

    implement reducer using ngrx schematics

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,35 +9,12 @@ import { environment } from '../../environments/environment';
 import { User } from '../model/user.model';
 import { AuthActionTypes } from '../auth/auth.actions';
 
-type AuthState = {
-  loggedIn: boolean,
-  user: User
-}
-
-const initialAuthState: AuthState = {
-  loggedIn: false,
-  user: undefined
-}
-
-function authReducer(state: AuthState = initialAuthState, action) : AuthState {
-  switch(action.type) {
-    case AuthActionTypes.LoginAction: 
-      return {
-        loggedIn: true,
-        user: action.payload.user
-      }
-
-    default: 
-      return state;
-  }
-}
- 
 export interface AppState {
-  auth: AuthState
+
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  auth: authReducer
+
 };
 
 
